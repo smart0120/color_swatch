41b913592d0df7ee283e97684578267f1d58ac2e
smart0120 <gadrgon0305@gmail.com>
2021-04-26 11:01:22 -0300

    feat: add colors view & genertate button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import './App.css';
+import {
+  Button
+} from 'reactstrap';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorList: [],
+    };
+  }
+
+  getColors = () => {
+    axios
+      .post("/get_colors/")
+      .then((res) => this.setState({ colorList: res.data }))
+      .catch((err) => console.log(err));
+  }
+
+  componentDidMount() {
+    this.getColors();
+  }
+
+  renderColors = () => {
+    
+    return (
+      this.state.colorList.map((color, idx) => {
+        const style = {
+          width: "200px",
+          height: "200px",
+        }
+        if (color.type === "rgb") {
+          style.backgroundColor = `rgb(${color.red}, ${color.green}, ${color.blue})`;
+        } else if (color.type === "hsl") {
+          style.backgroundColor = `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`;
+        }
+        return <h5 key={idx} style={style} className="d-flex align-items-center justify-content-center font-weight-bold text-white">{color.type}({color.red || color.hue}, {color.green || color.saturation}, {color.blue || color.lightness})</h5>
+      })
+    );
+  }
+
+  render() {
+    return (
+      <div className="App p-5">
+        <Button onClick={() => this.getColors()}>Generate colors</Button>
+        <div className="d-flex mt-5">
+          {this.renderColors()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
